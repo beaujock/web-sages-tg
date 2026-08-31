6521234ffa232a95debf54f4a7ecd2ae6d05ba82
Damigu Djagbare <djagbare2d@gmail.com>
2026-08-31 10:12:23 -0600

    auth works 1

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,6 +2,7 @@ import { jwtVerify } from 'jose';
 
 export const API_BASE_URL = process.env.NEXT_PUBLIC_SAGES_BASE_URL as string;
 export const JWT_SECRET = process.env.JWT_SECRET as string;
+console.log("Entering Auth to see JWT Secret : ", JWT_SECRET);
 
 type resourceCombo = {
   type_resource: string;
